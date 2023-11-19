186205bcea59bb9199deb82a5e95d6564493d179
LucasGetsCode <lucas.pujia@gmail.com>
2023-11-18 22:28:45 -0300

    Comienzo ej7

---
+++ AyED-guias/p7/colaprioridad/src/main/java/ej7/HeapArbolBinario.java
@@ -0,0 +1,18 @@
+package ej7;
+
+/**
+ * Hello world!
+ */
+public final class HeapArbolBinario {
+    private HeapArbolBinario() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ AyED-guias/p7/colaprioridad/src/main/java/ej7/HeapArray.java
@@ -0,0 +1,15 @@
+package ej7;
+
+public class HeapArray {
+    private HeapArray() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
